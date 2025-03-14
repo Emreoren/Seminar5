d6d7b4874fa65e51dd49ba848ef19c77680a9066
EmreOren <m.emreoren@gmail.com>
2025-03-14 15:52:45 +0200

    added retrive funcs to the serviceimpl

--- src/main/java/lv/venta/service/impl/ProductCRUDServiceImpl.java
+++ src/main/java/lv/venta/service/impl/ProductCRUDServiceImpl.java
@@ -1,5 +1,57 @@
 package lv.venta.service.impl;
 
-public class ProductCRUDServiceImpl {
+import java.util.ArrayList;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import lv.venta.model.Product;
+import lv.venta.repo.IProductRepo;
+import lv.venta.service.IProductCRUDService;
+
+@Service
+public class ProductCRUDServiceImpl implements IProductCRUDService{
+
+	@Autowired
+	private IProductRepo prodRepo;
+	
+	
+	
+	@Override
+	public void create(String inputTitle, float inputPrice, String inputDescriptrion, int inputQuantity)
+			throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public ArrayList<Product> retrieveAll() throws Exception {
+		if(prodRepo.count() == 0)
+		{
+			throw new Exception("Product table is empoty");
+		}
+		
+		ArrayList<Product> allProducts = (ArrayList<Product>) prodRepo.findAll();
+		return allProducts;
+	}
+
+	@Override
+	public Product retrieveByID(long id) throws Exception {
+		
+		if(prodRepo.findById())
+	}
+
+	@Override
+	public void UpdateById(long id, float inputPrice, int inputQuantity) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void DeleteById(long id) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+	
 
 }

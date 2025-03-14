73c44ea20e62f4ddbd66989e68f8cff06eba174d
EmreOren <m.emreoren@gmail.com>
2025-03-14 13:58:11 +0200

    added new annotations

--- src/main/java/lv/venta/model/Product.java
+++ src/main/java/lv/venta/model/Product.java
@@ -1,8 +1,18 @@
 package lv.venta.model;
 
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -13,14 +23,37 @@ import lombok.ToString;
 @Setter
 @NoArgsConstructor
 @ToString
+
+@Table(name = "ProductTable") // Mysql will be product_table, H2 - PRODUCT_TABLE
+@Entity
 public class Product {
 	//variables
-	private long id;	//TODO auto increment from DB
+	@Column(name = "Id")
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Setter(value = AccessLevel.NONE) // this means thet setter for id will be not visible/usable
+	private long id;
+	
 	@NotNull
 	@Pattern(regexp = "[A-Z]{1}[a-z]{2,20}")
+	@Column(name = "Title")
 	private String title;	
+	
+	@Min(0)
+	@Max(1000)
+	@Column(name = "Price")
 	private float price;
+	
+	@Column(name = "Description")
+	@NotNull
+	@Pattern(regexp = "[A-Za-z0-9 ,.;:]+")
+	@Size(min = 5, max = 100)
 	private String description;
+	
+	
+	@Min(0)
+	@Max(10000)
+	@Column(name = "Quantity")
 	private int quantity;
 	
 	
